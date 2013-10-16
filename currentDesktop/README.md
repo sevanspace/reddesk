7ea48614c304b10eaf49acaede488b06d553f592
Evan Hadfield <evanhadfield@gmail.com>
2013-10-16 17:23:29 -0500

    update threadded version to use working threads; ignore currentDesktop images

---
+++ currentDesktop/README.md
@@ -0,0 +1,7 @@
+# currentDesktop
+
+This folder holds images waiting to be set as the desktop background.
+
+This README is necessary for commiting this folder to Git, which is critical for the functioning of the application.
+
+Make sure you do NOT commit image files in this folder.
--- threddesk.py
+++ threddesk.py
@@ -15,19 +15,32 @@ from xml.etree import ElementTree as etree
 # subreddits
 subreddits = ["cityporn", "architectureporn", "infrastructureporn", "villageporn"]
 
+# default timeout
+default_timeout = 5
+
 # timeout in secs for downloading images
 image_download_timeout = 10
 
 # timeout in secs for parsing subreddits
 page_parse_timeout = 5
 
+# getImageLink threads
+image_builder_thread_count = 5
+
+# image downloading threads
+image_downloader_thread_count = 10
+
+# INITIALIZE
+
+# holds image objects parsed from reddit .rss feeds
+image_q = Queue.Queue()
 
 
-#INITIALIZE
 
-socket.setdefaulttimeout(timeout)
+# set urllib2 request timeout
+socket.setdefaulttimeout(default_timeout)
 
-
+# gets response from calling url or prints error output
 def urlRequest(url):
 	req = urllib2.Request(url)
 	try: 
@@ -48,51 +61,74 @@ def urlRequest(url):
 def buildXmlTree(url):
 	response = urlRequest(url)
 	xmltree = etree.fromstring(response)
-	
+	return xmltree
 
-	#something
-
+# fill image queue with images from subreddit feed
 def getImageLinks(subreddit):
-	# get XMLtree from url
-	# parse tree to find images
-	# put links to images into imageLink queue
-
 	url = "http://www.reddit.com/r/" + subreddit + ".rss"
 
-	try:
-		data = urlRequest(url)
+	xmltree = buildXmlTree(url)
 
-	
-	xtree = get
+	items = xmltree.findall('channel/item')
 
-reddit_file = urllib2.urlopen('url')
-#convert to string:
-reddit_data = reddit_file.read()
-print reddit_data
-#close file because we dont need it anymore:
-reddit_file.close()
+	for item in items:
+		image = RedditImage(item)
+		if (image):
+			image_q.put(image)
 
-#entire feed
-reddit_root = etree.fromstring(reddit_data)
-item = reddit_root.findall('channel/item')
-print item
+# RedditImage is instantiated with a Reddit xml 'item'
+# and stores parsed details about the item image
+class RedditImage:
+	def __init__(self, xmlItem):
+		self.__xmlItem = xmlItem
+		self.url = self.__url()
 
-reddit_feed=[]
-for entry in item:   
-    #get description, url, and thumbnail
-    desc = entry.findtext('description')  
-    reddit_feed.append([desc])
+		# if a valid link url can't be parsed, image is unusable
+		if self.url = "":
+			return None
+
+		self.title = self.__title()
+		self.dimensions = self.__dimensions()
+
+		def __title(self):
+			return self.__xmlItem.find('title').text
+
+		def __dimensions(self):
+			item_title = self.__xmlItem.find('title').text
+			m = re.search('([0-9]{3,5})[^0-9]+([0-9]{3,5})', item_title)
+			if m != None and m.lastindex != None and m.lastindex > 1:
+				return (int(m.group(1)), int(m.group(2)))
+			else:
+				return (0,0)
+
+		def __url(self):
+			item_description = self.xmlItem.find('description').text
+			m = re.search('<a href="([^"]+)">\[link\]', item_description)   
+			if m != None and m.lastindex != None and m.lastindex > 0:
+				return m.group(1)
+			else:
+				return ""
 
 
+# START WORKER THREADING
 
-worker_data = ['http://google.com', 'http://yahoo.com', 'http://bing.com']
+# runs function on objects in queue_start until empty
+def worker(queue_start, function):
+	queue_full = True
+	while queue_full:
+		try:
+			obj = queue.get(False)
+			function(obj)
 
+		except Queue.empty:
+			queue_full - False
 
-#C/P'ed from stack overflow
-#load up a queue with your data, this will handle locking
-q = Queue.Queue()
-for url in worker_data:
-    q.put(url)
+# RUNNING ENGINE
+
+for i in range(image_builder_thread_count):
+	t = threading.Thread(target=worker, args=(image_q,))
+	t.start()
+
 
 #define a worker function
 def worker(queue):
